1fc79babc9ae9a52f7fdb9adfebb3bc57c78ab30
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2020-05-13 23:49:29 +0800

    Display the added state

--- src/Restaurant/RestaurantList.js
+++ src/Restaurant/RestaurantList.js
@@ -2,14 +2,29 @@ import React from 'react';
 import RestaurantForm from './RestaurantForm';
 
 class RestaurantList extends React.Component {
+    state = { restaurants: [] };
+
     render() {
+        const { restaurants } = this.state;
         return (
             <div className="restaurant-list">
                 <button data-action="create">Add Restaurant</button>
-                <RestaurantForm />
+                <RestaurantForm onSave={this.onRestaurantSaveCommand} />
+                <ul>
+                    {restaurants.map(({ name }) => <RestaurantListItem key={name} name={name} />)}
+                </ul>
             </div>
         );
     }
+
+    onRestaurantSaveCommand = (restaurant) => {
+        const restaurants = [...this.state.restaurants, restaurant];
+        this.setState({ restaurants });
+    }
 }
+
+const RestaurantListItem = (props) => (
+    <li>{props.name}</li>
+);
 
 export default RestaurantList;
